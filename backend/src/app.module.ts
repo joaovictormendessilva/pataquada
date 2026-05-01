7adfd9498ea98e2f971342942dfbc21bf81d401e
joaovictormendessilva <joao_victor-08@hotmail.com>
2026-05-01 08:57:44 -0300

    setup(backend): Implement typeorm, @nestjs/typeorm and pg

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_DATABASE'),
+        synchronize: config.get<boolean>('DB_SYNCHRONIZE'),
+        logging: config.get<boolean>('DB_LOGGING'),
+        entities: [],
+      }),
     }),
   ],
 })
