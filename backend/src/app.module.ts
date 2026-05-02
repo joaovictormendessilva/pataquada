81189de0607d55179d98ede103bdc512dfeabc90
joaovictormendessilva <joao_victor-08@hotmail.com>
2026-05-01 21:51:35 -0300

    feature(backend): add create user process

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
 import { dataSourceOptions } from './database/data-source';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
